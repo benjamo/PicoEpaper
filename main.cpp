a6bc4efcf15e71540c61b875af57dfc0cd4ecefe
Benjamin Malbrich <benjamin.malbrich@gmail.com>
2023-04-10 14:56:04 +0200

    Add wait for USB ready

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <tusb.h>
 #include "lib/ePaper/epdspi.h"
 #include "lib/ePaper/gdey027t91.h"
 #include "logo.h"
@@ -14,11 +15,19 @@
 int main(void) {
 
 	stdio_init_all();
+
+	uint32_t t0 = time_us_32();
+    while (!tud_cdc_connected()) { 
+		sleep_ms(100);  
+	}
+    uint32_t t1 = time_us_32();
+    printf("\n%ldus\n", t1 - t0);
+
 	EpdSpi IO;
 	Gdey027T91 display(IO);
 	display.init();
 	display.DrawBitmap(logo);
 	display.update();
-	
+
     return 0;
 }
